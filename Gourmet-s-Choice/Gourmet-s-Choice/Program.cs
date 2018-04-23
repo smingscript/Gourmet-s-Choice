1b4f9c0088ef7945c5ce020845a6cb719b96c9d3
동건 신 <domingo23shin@gmail.com>
2018-04-23 18:23:59 +0900

    new

--- Gourmet-s-Choice/Gourmet-s-Choice/Program.cs
+++ Gourmet-s-Choice/Gourmet-s-Choice/Program.cs
@@ -14,6 +14,8 @@ namespace Gourmet_s_Choice
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
+
+//            new
         }
     }
 }
