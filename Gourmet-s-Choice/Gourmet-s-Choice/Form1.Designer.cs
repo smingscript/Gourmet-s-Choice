73cc7e1e99e8773901830b7cfd55660f30f541e4
수민 김 <strobe410glitter@gmail.com>
2018-04-24 11:05:11 +0900

    commit

--- Gourmet-s-Choice/Gourmet-s-Choice/Form1.Designer.cs
+++ Gourmet-s-Choice/Gourmet-s-Choice/Form1.Designer.cs
@@ -28,21 +28,49 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
             this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(12, 42);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(364, 435);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Location = new System.Drawing.Point(382, 42);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(379, 435);
+            this.pictureBox2.TabIndex = 1;
+            this.pictureBox2.TabStop = false;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(773, 489);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
     }
 }
 
--- Gourmet-s-Choice/Gourmet-s-Choice/Form1.cs
+++ Gourmet-s-Choice/Gourmet-s-Choice/Form1.cs
@@ -19,7 +19,19 @@ namespace Gourmet_s_Choice
 
         private void Form1_Load(object sender, EventArgs e)
         {
-            
+            //몇 강을 할지 선택하는 창이 띄운다
+
+
+        }
+
+        //랜덤으로 음식을 가져올 리스트를 생성해둔다
+        private List<int> RandomNumber = new List<int>();
+
+        //16강, 32강 선택
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            //이 창은 선택을 하면 두 토너먼트 선택 창을 숨기고, 랜덤 이미지의 인덱스를 리스트로 가져온다
+
         }
     }
 }
