225ffbfbf047bf75f9fd3fa17ee76c1e2bc8ddbe
수민 김 <strobe410glitter@gmail.com>
2018-04-24 10:08:00 +0900

    create random number

--- Gourmet-s-Choice/Gourmet-s-Choice/Form1.cs
+++ Gourmet-s-Choice/Gourmet-s-Choice/Form1.cs
@@ -16,5 +16,10 @@ namespace Gourmet_s_Choice
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+        }
     }
 }
