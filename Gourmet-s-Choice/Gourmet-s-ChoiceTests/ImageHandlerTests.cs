2743bcd535853ea9b58f78f4f5efeea733ed3aae
수민 김 <strobe410glitter@gmail.com>
2018-04-24 15:46:24 +0900

    resize unit test

---
+++ Gourmet-s-Choice/Gourmet-s-ChoiceTests/ImageHandlerTests.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Gourmet_s_Choice.Image;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gourmet_s_Choice.Image.Tests
+{
+    [TestClass()]
+    public class ImageHandlerTests
+    {
+        [TestMethod()]
+        public void ResizeTest()
+        {
+            Bitmap bitmap = (Bitmap) Bitmap.FromFile("C:\\Users\\KCCI\\Desktop\\6.The.Avengers.jpg");
+
+            Bitmap newBitmap = bitmap.Resize(600, 900, 1080, "C:\\Users\\KCCI\\Desktop\\6.The.Avengers2.jpg");
+
+        }
+    }
+}
