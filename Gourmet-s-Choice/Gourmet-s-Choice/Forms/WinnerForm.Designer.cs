7ec8064252a6a0dbea63c11de3d0d42cfea2ebc3
수민 김 <strobe410glitter@gmail.com>
2018-04-26 09:46:45 +0900

    convert

--- Gourmet-s-Choice/Gourmet-s-Choice/Forms/WinnerForm.Designer.cs
+++ Gourmet-s-Choice/Gourmet-s-Choice/Forms/WinnerForm.Designer.cs
@@ -28,32 +28,12 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.SuspendLayout();
-            // 
-            // textBox1
-            // 
-            this.textBox1.Location = new System.Drawing.Point(324, 200);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(127, 21);
-            this.textBox1.TabIndex = 0;
-            this.textBox1.Text = "You are the winner!";
-            // 
-            // WinnerForm
-            // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.components = new System.ComponentModel.Container();
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.textBox1);
-            this.Name = "WinnerForm";
             this.Text = "WinnerForm";
-            this.ResumeLayout(false);
-            this.PerformLayout();
-
         }
 
         #endregion
-
-        private System.Windows.Forms.TextBox textBox1;
     }
 }
